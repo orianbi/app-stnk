69d9cebf0461f7b08182e717d8b81d43e4aed160
orianbi <isaasrori32@gmail.com>
2021-06-29 08:33:25 +0800

    first commit app stnk

---
+++ website/static/js/myjs.js
@@ -0,0 +1,17 @@
+
+
+// $("#id_tgl_lahir").datetimepicker({ changeYear: true, changeMonth: true, dateFormat: 'yy-mm-dd H:m' });
+$("#id_tgl_lahir").datetimepicker({ changeYear: true, changeMonth: true, dateFormat: 'yy-mm-dd H:M' });
+$("#id_tgl_sweb").datepicker({ changeYear: true, changeMonth: true, dateFormat: 'yy-mm-dd' });
+$("#id_tgl_lapor_dikes").datepicker({ changeYear: true, changeMonth: true, dateFormat: 'yy-mm-dd' });
+$("#id_tgl_sweb_keluar").datepicker({ changeYear: true, changeMonth: true, dateFormat: 'yy-mm-dd' });
+$("#id_tgl_mrs").datepicker({ changeYear: true, changeMonth: true, dateFormat: 'yy-mm-dd' });
+$("#id_tgl_pulang").datepicker({ changeYear: true, changeMonth: true, dateFormat: 'yy-mm-dd' });
+
+// $('#id_tgl_lahir').datetimepicker({ Format: 'yy-mm-dd', });
+
+
+
+
+// $("#id_tgl_lahir").datetimepicker();
+
