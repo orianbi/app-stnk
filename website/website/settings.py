17e50767342133c9c114abd3f1ef34df77002ffc
orianbi <isaasrori32@gmail.com>
2021-07-11 14:54:41 +0800

    Menbahkan os.environ.get dan dj_databese_url di file setting.py

--- website/website/settings.py
+++ website/website/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from django.contrib import messages
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=f4mxu9tzb%$h)4#%-5n8(vggqxqe6&m_e2*w(p6xz+28v+azx'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-=f4mxu9tzb%$h)4#%-5n8(vggqxqe6&m_e2*w(p6xz+28v+azx') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1, localhost').split(',')
 
 
 # Application definition
@@ -82,6 +83,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+if os.environ.get('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
